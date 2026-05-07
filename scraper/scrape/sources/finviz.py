201ee53f671f33c7a2b139baa70c43b63754865d
nreHieW <limweihern@gmail.com>
2026-05-07 09:20:46 +0800

    finviz login

--- scraper/scrape/sources/finviz.py
+++ scraper/scrape/sources/finviz.py
@@ -10,24 +10,23 @@ from scrape.core.http_utils import fetch_html, get_htmls
 _PERF_COLUMNS = ["Perf Week", "Perf Month", "Perf Quarter", "Perf Half Y", "Perf Year", "Perf YTD"]
 
 
-def _parse_finviz_html(html: str, ticker: str) -> pd.DataFrame:
-    soup = BeautifulSoup(html, "lxml")
-    table = soup.find("table", class_="snapshot-table2")
-    if not table:
-        raise ValueError("No table found for ticker: " + ticker)
-    df = pd.read_html(StringIO(str(table)))[0].iloc[:, -2:].set_index(10)
-    df[11] = df[11].astype(str).str.replace("%", "")
-    perf_values = df.loc[_PERF_COLUMNS].astype(float).T.reset_index(drop=True)
-    indiv = pd.DataFrame(perf_values, columns=_PERF_COLUMNS)
-    indiv["Ticker"] = ticker
-    return indiv
-
-
 def parse_finviz(tickers):
     finviz_urls = ["https://finviz.com/quote.ashx?t=" + t for t in tickers]
     htmls = get_htmls(finviz_urls)
     dfs = []
 
     for i, ticker in enumerate(tickers):
-        dfs.append(_parse_finviz_html(htmls[i], ticker))
+        html = htmls[i]
+        soup = BeautifulSoup(html, "lxml")
+        table = soup.find("table", class_="snapshot-table2")
+        if not table:
+            # raise ValueError("No table found for ticker: " + ticker)
+            print(f"No table found for ticker: {ticker}")
+            continue
+        df = pd.read_html(StringIO(str(table)))[0].iloc[:, -2:].set_index(10)
+        df[11] = df[11].astype(str).str.replace("%", "")
+        perf_values = df.loc[_PERF_COLUMNS].astype(float).T.reset_index(drop=True)
+        indiv = pd.DataFrame(perf_values, columns=_PERF_COLUMNS)
+        indiv["Ticker"] = ticker
+        dfs.append(indiv)
     return pd.concat(dfs, axis=0, ignore_index=True).set_index("Ticker")

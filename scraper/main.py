af56904be28fc85fbb97425dbc9cb206ca776c87
nreHieW <limweihern@gmail.com>
2026-05-31 00:50:27 +0800

    fix scraper

--- scraper/main.py
+++ scraper/main.py
@@ -213,7 +213,7 @@ def run_comps_scrape(tickers, client, cached_yahoo_profiles=None, yahoo_snapshot
 
 def main():
     started = time.monotonic()
-    tickers = get_all_tickers()[:400]
+    tickers = get_all_tickers()
     logger.info("Tickers loaded: %s", len(tickers))
     client = get_mongo_client()
     load_marketscreener_cache()
